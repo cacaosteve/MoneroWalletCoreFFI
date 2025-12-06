5fd6095d443902663c53d42834bfd43fddb5bf45
steve <stevez@me.com@users.noreply.github.com>
2025-12-05 22:31:03 -0800

    fix

---
+++ monero-oxide-output/build.rs
@@ -0,0 +1,9 @@
+use std::env;
+
+fn main() {
+    let target_os = env::var("CARGO_CFG_TARGET_OS").unwrap_or_default();
+
+    if target_os == "ios" || target_os == "macos" {
+        println!("cargo:rustc-link-lib=c++");
+    }
+}
