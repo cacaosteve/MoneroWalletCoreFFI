ec6274ff952d53c47e7dc72aa28244c1667ad63c
steve <1307385+cacaosteve@users.noreply.github.com>
2026-08-20 21:10:07 -0700

    Point SwiftPM at WalletCore v0.1.2

--- Package.swift
+++ Package.swift
@@ -32,8 +32,8 @@ let package = Package(
             // versioned release asset. Keeping it out of Git makes clones and
             // SwiftPM dependency resolution much smaller while preserving the
             // no-Rust-required Apple consumer experience.
-            url: "https://github.com/cacaosteve/MoneroWalletCoreFFI/releases/download/walletcore-v0.1.1/MoneroWalletCore.xcframework.zip",
-            checksum: "28c96ef6a43d61eb56ecb5d4be0a82314622406b9ca0f9edc5f738ba5df9817f"
+            url: "https://github.com/cacaosteve/MoneroWalletCoreFFI/releases/download/walletcore-v0.1.2/MoneroWalletCore.xcframework.zip",
+            checksum: "b35d5d1d5cade8860f502e4eba1739605b954546cf8d81946d8e964228ce6853"
         ),
         clibTarget,
         .target(
